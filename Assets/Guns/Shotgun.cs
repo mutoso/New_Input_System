f3e307762f8b0c9c0a91be484de7bde8c0a11c5f
Alastair Feille <me@alastair.se>
2024-04-18 22:45:06 -0500

    Add shotgun

---
+++ Assets/Guns/Shotgun.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : Gun
+{
+    [SerializeField] GameObject shotgunBlastPrefab;
+
+    public override bool AttemptFire()
+    {
+        if (!base.AttemptFire())
+            return false;
+
+        var b = Instantiate(bulletPrefab, gunBarrelEnd.transform.position, gunBarrelEnd.rotation);
+        b.GetComponent<Projectile>().Initialize(10, 100, 0.5f, 25, null); // version without special effect
+
+        Instantiate(shotgunBlastPrefab, gunBarrelEnd.transform.position, gunBarrelEnd.rotation);
+
+        anim.SetTrigger("shoot");
+        elapsed = 0;
+        ammo -= 1;
+
+        return true;
+    }
+}
