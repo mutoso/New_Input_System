5069cf9c14fcf46ff5fa1803746126d24ff7d81e
Alastair Feille <me@alastair.se>
2024-04-20 16:04:39 -0500

    Add state boilerplate

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum EnemyStates
+{
+    Wander,
+    Pursue,
+    Attack,
+    Recovery
+}
+
 public class Enemy : MonoBehaviour
 {
     public Rigidbody Rigidbody { get; private set; }
     Vector3 origin;
+    EnemyStates state;
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +25,40 @@ public class Enemy : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        switch (state)
+        {
+            case EnemyStates.Wander:
+                UpdateWander();
+                break;
+            case EnemyStates.Pursue:
+                UpdatePursue();
+                break;
+            case EnemyStates.Attack:
+                UpdateAttack();
+                break;
+            case EnemyStates.Recovery:
+                UpdateRecovery();
+                break;
+        }
+    }
+
+    void UpdateWander()
+    {
+
+    }
+
+    void UpdatePursue()
+    {
+
+    }
+
+    void UpdateAttack()
+    {
+
+    }
+
+    void UpdateRecovery()
     {
 
     }
